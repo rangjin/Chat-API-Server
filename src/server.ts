637baae1e745278896e5229dc18526f513988466
rangjin <rangjin2037@gmail.com>
2025-08-23 03:05:07 +0900

    script 설정

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.json({ success: true });
+});
+
+app.listen(port, () => {
+  console.log(`server is listening at localhost:${process.env.PORT}`);
+});
